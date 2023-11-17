0041820e4a1cb066743afae14f5cdeceb82ef323
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-17 13:27:36 +0100

    Aggiungere i file di progetto.

---
+++ SharingVictory/Program.cs
@@ -0,0 +1,47 @@
+﻿using SharingVictory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        /**
+         * Se andiamo a inserire in una Lista un altro Studente con nome differente ma con stesso valore voto 
+         * l'applicazione mi darà come risultato solo lo studente che viene per ultimo in lista con quel valore.
+         */
+        List<Studente> studenti = new List<Studente>()
+        {
+            new Studente("Marco", 10),
+            new Studente("Lorenzo", 10),
+            new Studente("Alessio", 3),
+            new Studente("Alessia", 7),
+            new Studente("Marta", 6),
+            new Studente("Stefano", 4),
+            new Studente("Francesca", 5),
+            new Studente("Mirco", 9),
+            new Studente("Emanuela", 8),
+            new Studente("Paolo", 6),
+            new Studente("Pamela", 2)
+    };
+
+
+        //---------------------------------------------------------------------------------------------
+
+        double media = studenti.Average(studente => studente.voto);
+
+        Console.WriteLine($"Media Voti: {media}\n");
+
+        //---------------------------------------------------------------------------------------------
+
+        List<Studente> topTwoStudents2 = studenti.OrderByDescending(studente => studente.voto).
+            ThenBy(studente => studente.nome).Take(2).ToList();
+
+        string risultato = string.Join(", ", topTwoStudents2.Select(studente => studente.ToString()));
+
+        
+        Console.WriteLine($"Gli studenti con il voto più alto sono: {risultato}\n");
+        
+
+
+    }
+}
---
+++ SharingVictory/Studente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharingVictory
+{
+    internal class Studente
+    {
+        public string? nome { get; set; }
+        public int voto { get; set; }
+
+
+        public Studente(string? nome, int voto)
+        {
+            this.nome = nome;
+            this.voto = voto;
+        }
+
+        public override string ToString()
+        {
+            return $"{nome}: {voto}";
+        }
+    }
+}
